96fb62a177e070adc37d9bf011b42353cf8d642e
johanluk <joluk11@gmail.com>
2023-06-27 20:26:02 +0700

    Customer trial

--- Master Data/Master Data/Frm_Customer.Designer.vb
+++ Master Data/Master Data/Frm_Customer.Designer.vb
@@ -20,19 +20,16 @@ Partial Class Frm_Customer
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim CustomerNoLabel As System.Windows.Forms.Label
         Me.RibbonControl = New DevExpress.XtraBars.Ribbon.RibbonControl()
         Me.RibbonPage1 = New DevExpress.XtraBars.Ribbon.RibbonPage()
         Me.RibbonPageGroup1 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
         Me.RibbonStatusBar = New DevExpress.XtraBars.Ribbon.RibbonStatusBar()
         Me.LayoutControl1 = New DevExpress.XtraLayout.LayoutControl()
-        Me.LayoutControlGroup1 = New DevExpress.XtraLayout.LayoutControlGroup()
-        Me.DataSet = New Master_Data.DataSet()
-        Me.Sp_MDCustomer_GetDataBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.Sp_MDCustomer_GetDataTableAdapter = New Master_Data.DataSetTableAdapters.sp_MDCustomer_GetDataTableAdapter()
-        Me.TableAdapterManager = New Master_Data.DataSetTableAdapters.TableAdapterManager()
         Me.Sp_MDCustomer_GetDataGridControl = New DevExpress.XtraGrid.GridControl()
+        Me.Sp_MDCustomer_GetDataBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DataSet = New Master_Data.DataSet()
         Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-        Me.LayoutControlItem1 = New DevExpress.XtraLayout.LayoutControlItem()
         Me.colCustomerNo = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.colCustomerName = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.colCustomerTypeID = New DevExpress.XtraGrid.Columns.GridColumn()
@@ -62,20 +59,28 @@ Partial Class Frm_Customer
         Me.colModifiedUser = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.colModifiedDate = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.LayoutControlGroup1 = New DevExpress.XtraLayout.LayoutControlGroup()
+        Me.LayoutControlItem1 = New DevExpress.XtraLayout.LayoutControlItem()
+        Me.Sp_MDCustomer_GetDataTableAdapter = New Master_Data.DataSetTableAdapters.sp_MDCustomer_GetDataTableAdapter()
+        Me.TableAdapterManager = New Master_Data.DataSetTableAdapters.TableAdapterManager()
         Me.DockManager1 = New DevExpress.XtraBars.Docking.DockManager(Me.components)
         Me.DockPanel1 = New DevExpress.XtraBars.Docking.DockPanel()
         Me.DockPanel1_Container = New DevExpress.XtraBars.Docking.ControlContainer()
+        Me.CustomerNoTextEdit = New DevExpress.XtraEditors.TextEdit()
+        CustomerNoLabel = New System.Windows.Forms.Label()
         CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.LayoutControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.LayoutControl1.SuspendLayout()
-        CType(Me.LayoutControlGroup1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Sp_MDCustomer_GetDataBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Sp_MDCustomer_GetDataGridControl, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Sp_MDCustomer_GetDataBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LayoutControlGroup1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.LayoutControlItem1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DockManager1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.DockPanel1.SuspendLayout()
+        Me.DockPanel1_Container.SuspendLayout()
+        CType(Me.CustomerNoTextEdit.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'RibbonControl
@@ -87,7 +92,7 @@ Partial Class Frm_Customer
         Me.RibbonControl.Name = "RibbonControl"
         Me.RibbonControl.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.RibbonPage1})
         Me.RibbonControl.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2019
-        Me.RibbonControl.Size = New System.Drawing.Size(967, 169)
+        Me.RibbonControl.Size = New System.Drawing.Size(1367, 169)
         Me.RibbonControl.StatusBar = Me.RibbonStatusBar
         '
         'RibbonPage1
@@ -106,7 +111,7 @@ Partial Class Frm_Customer
         Me.RibbonStatusBar.Location = New System.Drawing.Point(0, 577)
         Me.RibbonStatusBar.Name = "RibbonStatusBar"
         Me.RibbonStatusBar.Ribbon = Me.RibbonControl
-        Me.RibbonStatusBar.Size = New System.Drawing.Size(967, 22)
+        Me.RibbonStatusBar.Size = New System.Drawing.Size(1367, 22)
         '
         'LayoutControl1
         '
@@ -115,44 +120,9 @@ Partial Class Frm_Customer
         Me.LayoutControl1.Location = New System.Drawing.Point(0, 169)
         Me.LayoutControl1.Name = "LayoutControl1"
         Me.LayoutControl1.Root = Me.LayoutControlGroup1
-        Me.LayoutControl1.Size = New System.Drawing.Size(767, 408)
+        Me.LayoutControl1.Size = New System.Drawing.Size(1167, 408)
         Me.LayoutControl1.TabIndex = 2
         Me.LayoutControl1.Text = "LayoutControl1"
-        '
-        'LayoutControlGroup1
-        '
-        Me.LayoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.[True]
-        Me.LayoutControlGroup1.GroupBordersVisible = False
-        Me.LayoutControlGroup1.Items.AddRange(New DevExpress.XtraLayout.BaseLayoutItem() {Me.LayoutControlItem1})
-        Me.LayoutControlGroup1.Name = "LayoutControlGroup1"
-        Me.LayoutControlGroup1.Size = New System.Drawing.Size(767, 408)
-        Me.LayoutControlGroup1.TextVisible = False
-        '
-        'DataSet
-        '
-        Me.DataSet.DataSetName = "DataSet"
-        Me.DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'Sp_MDCustomer_GetDataBindingSource
-        '
-        Me.Sp_MDCustomer_GetDataBindingSource.DataMember = "sp_MDCustomer_GetData"
-        Me.Sp_MDCustomer_GetDataBindingSource.DataSource = Me.DataSet
-        '
-        'Sp_MDCustomer_GetDataTableAdapter
-        '
-        Me.Sp_MDCustomer_GetDataTableAdapter.ClearBeforeFill = True
-        '
-        'TableAdapterManager
-        '
-        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
-        Me.TableAdapterManager.sp_MDEmployee_GetDataTableAdapter = Nothing
-        Me.TableAdapterManager.sp_MDInvBrand_GetDataTableAdapter = Nothing
-        Me.TableAdapterManager.sp_MDInvCategory_GetDataTableAdapter = Nothing
-        Me.TableAdapterManager.sp_MDInventory_GetDataTableAdapter = Nothing
-        Me.TableAdapterManager.sp_MDInventory_PrintBarcodeListTableAdapter = Nothing
-        Me.TableAdapterManager.sp_MDInvProduct_GetDataTableAdapter = Nothing
-        Me.TableAdapterManager.sp_T_Opname_GetDataTableAdapter = Nothing
-        Me.TableAdapterManager.UpdateOrder = Master_Data.DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
         '
         'Sp_MDCustomer_GetDataGridControl
         '
@@ -161,24 +131,25 @@ Partial Class Frm_Customer
         Me.Sp_MDCustomer_GetDataGridControl.MainView = Me.GridView1
         Me.Sp_MDCustomer_GetDataGridControl.MenuManager = Me.RibbonControl
         Me.Sp_MDCustomer_GetDataGridControl.Name = "Sp_MDCustomer_GetDataGridControl"
-        Me.Sp_MDCustomer_GetDataGridControl.Size = New System.Drawing.Size(743, 384)
+        Me.Sp_MDCustomer_GetDataGridControl.Size = New System.Drawing.Size(1143, 384)
         Me.Sp_MDCustomer_GetDataGridControl.TabIndex = 4
         Me.Sp_MDCustomer_GetDataGridControl.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
+        '
+        'Sp_MDCustomer_GetDataBindingSource
+        '
+        Me.Sp_MDCustomer_GetDataBindingSource.DataMember = "sp_MDCustomer_GetData"
+        Me.Sp_MDCustomer_GetDataBindingSource.DataSource = Me.DataSet
+        '
+        'DataSet
+        '
+        Me.DataSet.DataSetName = "DataSet"
+        Me.DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'GridView1
         '
         Me.GridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colCustomerNo, Me.colCustomerName, Me.colCustomerTypeID, Me.colIDCard, Me.colAddress, Me.colCity, Me.colPostCode, Me.colHamlet, Me.colNeighbourhood, Me.colVillage, Me.colSubdistrict, Me.colBirthDate, Me.colBirthPlace, Me.colReligion, Me.colGender, Me.colDescription, Me.colPhone1, Me.colPhone2, Me.colFax, Me.colimages, Me.colCredit_Limit, Me.colContact_Person, Me.colContact_Person_Address, Me.colContact_Person_Phone1, Me.colContact_Person_Phone2, Me.colemail, Me.colModifiedUser, Me.colModifiedDate, Me.colID})
         Me.GridView1.GridControl = Me.Sp_MDCustomer_GetDataGridControl
         Me.GridView1.Name = "GridView1"
-        '
-        'LayoutControlItem1
-        '
-        Me.LayoutControlItem1.Control = Me.Sp_MDCustomer_GetDataGridControl
-        Me.LayoutControlItem1.Location = New System.Drawing.Point(0, 0)
-        Me.LayoutControlItem1.Name = "LayoutControlItem1"
-        Me.LayoutControlItem1.Size = New System.Drawing.Size(747, 388)
-        Me.LayoutControlItem1.TextSize = New System.Drawing.Size(0, 0)
-        Me.LayoutControlItem1.TextVisible = False
         '
         'colCustomerNo
         '
@@ -389,6 +360,42 @@ Partial Class Frm_Customer
         Me.colID.OptionsColumn.AllowEdit = False
         Me.colID.OptionsColumn.AllowFocus = False
         '
+        'LayoutControlGroup1
+        '
+        Me.LayoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.[True]
+        Me.LayoutControlGroup1.GroupBordersVisible = False
+        Me.LayoutControlGroup1.Items.AddRange(New DevExpress.XtraLayout.BaseLayoutItem() {Me.LayoutControlItem1})
+        Me.LayoutControlGroup1.Name = "LayoutControlGroup1"
+        Me.LayoutControlGroup1.Size = New System.Drawing.Size(1167, 408)
+        Me.LayoutControlGroup1.TextVisible = False
+        '
+        'LayoutControlItem1
+        '
+        Me.LayoutControlItem1.Control = Me.Sp_MDCustomer_GetDataGridControl
+        Me.LayoutControlItem1.Location = New System.Drawing.Point(0, 0)
+        Me.LayoutControlItem1.Name = "LayoutControlItem1"
+        Me.LayoutControlItem1.Size = New System.Drawing.Size(1147, 388)
+        Me.LayoutControlItem1.TextSize = New System.Drawing.Size(0, 0)
+        Me.LayoutControlItem1.TextVisible = False
+        '
+        'Sp_MDCustomer_GetDataTableAdapter
+        '
+        Me.Sp_MDCustomer_GetDataTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.Connection = Nothing
+        Me.TableAdapterManager.sp_MDCustomer_GetDataTableAdapter = Nothing
+        Me.TableAdapterManager.sp_MDEmployee_GetDataTableAdapter = Nothing
+        Me.TableAdapterManager.sp_MDInvBrand_GetDataTableAdapter = Nothing
+        Me.TableAdapterManager.sp_MDInvCategory_GetDataTableAdapter = Nothing
+        Me.TableAdapterManager.sp_MDInventory_GetDataTableAdapter = Nothing
+        Me.TableAdapterManager.sp_MDInventory_PrintBarcodeListTableAdapter = Nothing
+        Me.TableAdapterManager.sp_MDInvProduct_GetDataTableAdapter = Nothing
+        Me.TableAdapterManager.sp_T_Opname_GetDataTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = Master_Data.DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
         'DockManager1
         '
         Me.DockManager1.Form = Me
@@ -400,7 +407,7 @@ Partial Class Frm_Customer
         Me.DockPanel1.Controls.Add(Me.DockPanel1_Container)
         Me.DockPanel1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Right
         Me.DockPanel1.ID = New System.Guid("3f4d6204-db9f-4943-8004-d5c772f93c7b")
-        Me.DockPanel1.Location = New System.Drawing.Point(767, 169)
+        Me.DockPanel1.Location = New System.Drawing.Point(1167, 169)
         Me.DockPanel1.Name = "DockPanel1"
         Me.DockPanel1.OriginalSize = New System.Drawing.Size(200, 200)
         Me.DockPanel1.Size = New System.Drawing.Size(200, 408)
@@ -408,17 +415,38 @@ Partial Class Frm_Customer
         '
         'DockPanel1_Container
         '
+        Me.DockPanel1_Container.AutoScroll = True
+        Me.DockPanel1_Container.Controls.Add(CustomerNoLabel)
+        Me.DockPanel1_Container.Controls.Add(Me.CustomerNoTextEdit)
         Me.DockPanel1_Container.Location = New System.Drawing.Point(4, 46)
         Me.DockPanel1_Container.Name = "DockPanel1_Container"
         Me.DockPanel1_Container.Size = New System.Drawing.Size(193, 359)
         Me.DockPanel1_Container.TabIndex = 0
+        '
+        'CustomerNoLabel
+        '
+        CustomerNoLabel.AutoSize = True
+        CustomerNoLabel.Location = New System.Drawing.Point(61, 86)
+        CustomerNoLabel.Name = "CustomerNoLabel"
+        CustomerNoLabel.Size = New System.Drawing.Size(87, 16)
+        CustomerNoLabel.TabIndex = 0
+        CustomerNoLabel.Text = "Customer No:"
+        '
+        'CustomerNoTextEdit
+        '
+        Me.CustomerNoTextEdit.DataBindings.Add(New System.Windows.Forms.Binding("EditValue", Me.Sp_MDCustomer_GetDataBindingSource, "CustomerNo", True))
+        Me.CustomerNoTextEdit.Location = New System.Drawing.Point(154, 83)
+        Me.CustomerNoTextEdit.MenuManager = Me.RibbonControl
+        Me.CustomerNoTextEdit.Name = "CustomerNoTextEdit"
+        Me.CustomerNoTextEdit.Size = New System.Drawing.Size(100, 22)
+        Me.CustomerNoTextEdit.TabIndex = 1
         '
         'Frm_Customer
         '
         Me.AllowFormGlass = DevExpress.Utils.DefaultBoolean.[True]
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(967, 599)
+        Me.ClientSize = New System.Drawing.Size(1367, 599)
         Me.Controls.Add(Me.LayoutControl1)
         Me.Controls.Add(Me.DockPanel1)
         Me.Controls.Add(Me.RibbonStatusBar)
@@ -430,14 +458,17 @@ Partial Class Frm_Customer
         CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.LayoutControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.LayoutControl1.ResumeLayout(False)
-        CType(Me.LayoutControlGroup1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataSet, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Sp_MDCustomer_GetDataBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Sp_MDCustomer_GetDataGridControl, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Sp_MDCustomer_GetDataBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LayoutControlGroup1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.LayoutControlItem1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DockManager1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.DockPanel1.ResumeLayout(False)
+        Me.DockPanel1_Container.ResumeLayout(False)
+        Me.DockPanel1_Container.PerformLayout()
+        CType(Me.CustomerNoTextEdit.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -488,4 +519,5 @@ Partial Class Frm_Customer
     Friend WithEvents DockManager1 As DevExpress.XtraBars.Docking.DockManager
     Friend WithEvents DockPanel1 As DevExpress.XtraBars.Docking.DockPanel
     Friend WithEvents DockPanel1_Container As DevExpress.XtraBars.Docking.ControlContainer
+    Friend WithEvents CustomerNoTextEdit As DevExpress.XtraEditors.TextEdit
 End Class
